52620cdf17ab9793ca4c3f08d9c89d1f4cec742a
chedifier <chedifier123@gmail.com>
2017-04-23 10:15:19 +0800

    add unexpect resolve of ABCDEF ACDBEF AEBCDF

---
+++ TopoResolver/src/casetest/BookmarkCase3_modify_conflict3.java
@@ -0,0 +1,53 @@
+package casetest;
+
+public class BookmarkCase3_modify_conflict3 extends TestCase{
+
+	@Override
+	public void prepareCase() {
+		caseData.add(new TestBookmarkBuilder("A")
+				.dirty(0)
+				.local_next("E")				
+				.order_time(3L)
+				.cloud_next("C")
+				.cloud_order_time(2L)
+				.build());
+		
+		caseData.add(new TestBookmarkBuilder("B")
+				.dirty(0)
+				.local_next("C")
+				.order_time(1L)
+				.cloud_next("E")
+				.cloud_order_time(2L)
+				.build());
+		
+		caseData.add(new TestBookmarkBuilder("C")
+				.dirty(0)
+				.local_next("D")
+				.order_time(1L)
+				.cloud_next("D")
+				.cloud_order_time(1L)
+				.build());
+		caseData.add(new TestBookmarkBuilder("D")
+				.dirty(0)
+				.local_next("F")
+				.order_time(3L)
+				.cloud_next("B")
+				.cloud_order_time(2L)
+				.build());
+		caseData.add(new TestBookmarkBuilder("E")
+				.dirty(0)
+				.local_next("B")
+				.order_time(3L)
+				.cloud_next("F")
+				.cloud_order_time(1L)
+				.build());
+		caseData.add(new TestBookmarkBuilder("F")
+				.dirty(0)
+				.local_next(null)
+				.order_time(1L)
+				.cloud_next(null)
+				.cloud_order_time(1L)
+				.build());
+	}
+
+}
--- TopoResolver/src/casetest/BookmarkResovlerTest.java
+++ TopoResolver/src/casetest/BookmarkResovlerTest.java
@@ -6,20 +6,27 @@ public class BookmarkResovlerTest {
 	
 	public void test(){
 		
-		new BookmarkCase1_local_add_middle().run();
-		new BookmarkCase1_local_add_top().run();
-		new BookmarkCase1_local_add().run();
-		new BookmarkCase1_local_del().run();
-		new BookmarkCase1_local_modify().run();
-		new BookmarkCase2_cloud_add_middle().run();
-		new BookmarkCase2_cloud_del().run();
-		new BookmarkCase3_modify_conflict().run();
-		new BookmarkCase3_modify_conflict2().run();
-		new BookmarkCase3_modify_noneconflict().run();
-		new BookmarkCase4_clouderror_circle().run();
-		new BookmarkCase4_clouderror_partly().run();
-		new BookmarkCase5_cadd_lmodify().run();
-		new BookmarkCase5_cloud_local_add().run();
+//		new BookmarkCase1_local_add_middle().run();
+//		new BookmarkCase1_local_add_top().run();
+//		new BookmarkCase1_local_add().run();
+//		new BookmarkCase1_local_del().run();
+//		new BookmarkCase1_local_modify().run();
+//		
+//		
+//		new BookmarkCase2_cloud_add_middle().run();
+//		new BookmarkCase2_cloud_del().run();
+		
+		
+//		new BookmarkCase3_modify_conflict().run();
+//		new BookmarkCase3_modify_conflict2().run();
+//		new BookmarkCase3_modify_noneconflict().run();
+		new BookmarkCase3_modify_conflict3().run();
+		
+		
+//		new BookmarkCase4_clouderror_circle().run();
+//		new BookmarkCase4_clouderror_partly().run();
+//		new BookmarkCase5_cadd_lmodify().run();
+//		new BookmarkCase5_cloud_local_add().run();
 
 	}
 	
